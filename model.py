9e98a4f5f8382fc38a3cd93a7d615ef07fa090d8
Sirish Gurung <sgurung7218@floridapoly.edu>
2024-11-26 00:42:56 -0500

    Some changes to the setup

--- model.py
+++ model.py
@@ -5,45 +5,48 @@ from scipy.signal import stft
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
-from view import *
-# y is a one-dimensional array, y(t) corresponds to amplitude at t
-# sr is the sampling rate
-sr, y = wavfile.read("test_audio.wav")
+import re
+# self.y is a one-dimensional arraself.y, self.y(t) corresponds to amplitude at t
+# self.sr is the sampling rate
+class Model:
 
-def freq_graph():
-    D = stft(y)  # STFT of y
-    s_db = np.abs(D)
-    return s_db, sr
-
-def compute_resonance(y,sr):
-    #Computing Short-Time Fourier Transform (STFT)
-    D = librosa.stft(y)
-    #Convert to magnitude spectrogram (absolute value of STFT)
-    magnitude, _ = librosa.magphase(D)
-    #Compute average magnitude
-    average_magnitude = np.mean(magnitude, axis=1)
-    #Compute frequency bins for respective STFT
-    frequencies = librosa.fft_frequencies(sr)
-    #the maximum magnitude in average magnitude spectrum
-    index_of_max_resonance = np.argmax(average_magnitude)
-    highest_resonance = frequencies[index_of_max_resonance]
-    return highest_resonance
-
-def calculate_mid_freq():
-    frequency = librosa.stft(y)
-    mid_freq_range = range(50, 1000)
-    mid_freq_data = abs(frequency[mid_freq_range, :])
-    return mid_freq_data, sr
-
-def compute_LMH(y,sr):
-    frequency = librosa.stft(y)
-    #finding the dimensions of matrix 'frequency' specifically the no. of rows
-    numOfBins = frequency.shape[0]
-    #Grouping frequency in ranges
-    low_freq_range = range(0,50)
-    mid_freq_range = range(50,1000)
-    high_freq_range = range (1000,20000)
-    #extracting data for each range
-    low_freq_data = abs(frequency[low_freq_range,:])
-    mid_freq_data = abs(frequency[mid_freq_range,:])
-    high_freq_data = abs(frequency[high_freq_range,:])
+    def __init__(self, y, sr):
+        self.y = y
+        self.sr = sr
+    def freq_graph(self):
+        D = stft(self.y)  # STFT of self.y
+        s_db = np.abs(D)
+        return s_db, self.sr
+    
+    def compute_resonance(self):
+        #Computing Short-Time Fourier Transform (STFT)
+        D = librosa.stft(self.y)
+        #Convert to magnitude spectrogram (absolute value of STFT)
+        magnitude, _ = librosa.magphase(D)
+        #Compute average magnitude
+        average_magnitude = np.mean(magnitude, axis=1)
+        #Compute frequencself.y bins for respective STFT
+        frequencies = librosa.fft_frequencies(self.sr)
+        #the maximum magnitude in average magnitude spectrum
+        index_of_max_resonance = np.argmax(average_magnitude)
+        highest_resonance = frequencies[index_of_max_resonance]
+        return highest_resonance
+    
+    def calculate_mid_freq(self):
+        frequencself.y = librosa.stft(self.y)
+        mid_freq_range = range(50, 1000)
+        mid_freq_data = abs(frequencself.y[mid_freq_range, :])
+        return mid_freq_data, self.sr
+    
+    def compute_LMH(self):
+        frequencself.y = librosa.stft(self.y)
+        #finding the dimensions of matrix 'frequencself.y' specificallself.y the no. of rows
+        numOfBins = frequencself.y.shape[0]
+        #Grouping frequencself.y in ranges
+        low_freq_range = range(0,50)
+        mid_freq_range = range(50,1000)
+        high_freq_range = range (1000,20000)
+        #extracting data for each range
+        low_freq_data = abs(frequencself.y[low_freq_range,:])
+        mid_freq_data = abs(frequencself.y[mid_freq_range,:])
+        high_freq_data = abs(frequencself.y[high_freq_range,:])
--- view.py
+++ view.py
@@ -9,137 +9,131 @@ import librosa.display
 
 #ONLY GRAPH BUTTON WORKS RIGHT NOW
 
-def plt_graph():
-    y, sr = mm.freq_graph()
-    for widget in _img_frame.winfo_children():
-        widget.destroy()
-    frequencies = librosa.fft_frequencies(sr=sr)
-    fig, ax=plt.subplots(figsize=(5,3))
-    ax.plot(y, sr)
-    ax.set_title('Waveform of the Audio')
-    ax.set_xlabel('Time(s)')
-    ax.set_ylabel('Amplitude')
-    ax.grid(True, linestyle = '--', color = 'gray', linewidth = 0.5)
-    fig.tight_layout()
-    canvas = FigureCanvasTkAgg(fig, master=_img_frame)
-    canvas.draw()
-    canvas.get_tk_widget().grid(row=1,column=0,padx=10,pady=10)
+class View(ttk.Frame):
+    def __init__(self,parent):
+        super().__init__(parent)
 
-def funct1():
-    print("funct1 test")
+        self.mainframe = ttk.Frame(self, padding='5 5 5 5 ')  # root is parent of frame
+        self.mainframe.grid(row=0, column=0, sticky=("E", "W", "N", "S"))  # placed on first row,col of parent
+        # frame can extend itself in all cardinal directions
+        self._url_frame = ttk.LabelFrame(self.mainframe, text='File Path', padding='5 5 5 5')  # label frame
+        self._url_frame.grid(row=0, column=0, sticky=("E", "W"))  # only expands E W
+        self._url_frame.columnconfigure(0, weight=1)
+        self._url_frame.rowconfigure(0, weight=1)  # behaves when resizing
 
+        self._load_btn = ttk.Button(self._url_frame, text='Load file', command=self.funct1)  # create button
+        # fetch_url() is callback for button press
+        self._load_btn.grid(row=0, column=1, sticky=W, padx=5)
 
-def funct2():
-    mid_freq_data, sr = mm.calculate_mid_freq()
-    for widget in _img_frame.winfo_children():
-        widget.destroy()
-    # Create a figure and axis
-    fig, ax = plt.subplots(figsize=(4,2))
-    # Plot the data
-    librosa.display.specshow(mid_freq_data, sr=sr, x_axis='time', y_axis='log')
-    # Set title and labels
-    ax.set_title("Graph Plot")
-    ax.set_xlabel("Amplitude")
-    ax.set_ylabel("Time(s)")
-    ax.grid(True, linestyle = '--', color = 'gray', linewidth = 0.5)
-    # Embed the plot in the Tkinter window
-    canvas = FigureCanvasTkAgg(fig, master=_img_frame)  # Create canvas
-    canvas.draw()
-    canvas.get_tk_widget().grid(row=1,column=0,padx=10,pady=10)
+        self._analyze_btn = ttk.Button(self._url_frame, text='Analyze', command=self.plt_graph)  # create button
+        # fetch_url() is callback for button press
+        self._analyze_btn.grid(row=0, column=2, sticky=W, padx=5)
 
+        self._save_method = StringVar()
+        self._save_method.set('img')
+        self._img_only_radio = ttk.Radiobutton(self._url_frame, text='File is .wav', variable=self._save_method,
+                                               value='img')
+        self._img_only_radio.grid(row=1, column=0, padx=5, pady=2, sticky="W")
+        self._img_only_radio.configure(state='normal')
+        self._json_radio = ttk.Radiobutton(self._url_frame, text='Convert to .wav', variable=self._save_method,
+                                           value='json')
+        self._json_radio.grid(row=2, column=0, padx=5, pady=2, sticky="W")
 
-def funct3():
-    print("funct3 test")
+        self._graph_frame = ttk.Frame(self.mainframe, padding="10")
+        self._graph_frame.grid(row=1, column=0, padx=10, pady=10)
+        self._graph_label = ttk.Label(self._graph_frame, text="Data analysis")
+        self._graph_label.grid(row=0, column=0)
+        # Create an initial empty plot
+        fig, ax = plt.subplots(figsize=(4, 2))  # Create a blank figure and axis
+        ax.set_title("Graphs")
+        ax.set_xlabel("Time")
+        ax.set_ylabel("Frequency")
+        ax.grid(True)
+        fig.tight_layout()
 
+        # Embed the initial empty plot into Tkinter
+        canvas = FigureCanvasTkAgg(fig, master=self._graph_frame)
+        canvas.draw()  # Draw the empty plot
+        canvas.get_tk_widget().grid(row=1, column=0, padx=10, pady=10)  # Add the canvas to the grid
 
-def save():
-    print("save test")
+        self._data_lbl = ttk.Label(self._graph_frame, text="Data")
+        self._data_lbl.grid(row=2, column=0, padx=5, pady=5)
 
+        self._btn_frame = ttk.Frame(self.mainframe, padding=(0, 10, 0, 0))
+        self._btn_frame.grid(row=1, column=1)
 
-def sb(msg):
-    _status_msg.set(msg)
+        self._low_btn = ttk.Button(self._btn_frame, text='Low', command=self.funct1)  # create button
+        # fetch_url() is callback for button press
+        self._low_btn.grid(row=0, column=0, sticky=W, padx=5)
+        # creates fetch title button
+        self._mid_btn = ttk.Button(self._btn_frame, text='Medium', command=self.funct2)  # create button
+        # fetch_url() is callback for button press
+        self._mid_btn.grid(row=1, column=0, sticky=W, padx=5)
 
+        # creates fetch link button
+        self._high_link_btn = ttk.Button(self._btn_frame, text='High', command=self.funct3)  # create button
+        # fetch_url() is callback for button press
+        self._high_link_btn.grid(row=2, column=0, sticky=W, padx=5)
 
-def alert(msg):
-    messagebox.showinfo(message=msg)
+        self._status_frame = ttk.Frame(self, relief='sunken', padding='2 2 2 2')
+        self._status_frame.grid(row=1, column=0, sticky=("E", "W", "S",))
+        self._status_msg = StringVar()  # need modified when update status text
+        self._status_msg.set('Type a file path to select a file...')
+        self._status = ttk.Label(self._status_frame, textvariable=self._status_msg, anchor=W)
+        self._status.grid(row=0, column=0, sticky=(E, W))
 
+    def set_controller(self, controller):
+        """
+        Set the controller
+        :param controller:
+        :return:
+        """
+        self.controller = controller
+    def plt_graph(self):
+        y, sr = mm.freq_graph()
+        for widget in self._graph_frame.winfo_children():
+            widget.destroy()
+        frequencies = librosa.fft_frequencies(sr=sr)
+        fig, ax = plt.subplots(figsize=(5, 3))
+        ax.plot(y, sr)
+        ax.set_title('Waveform of the Audio')
+        ax.set_xlabel('Time(s)')
+        ax.set_ylabel('Amplitude')
+        ax.grid(True, linestyle='--', color='gray', linewidth=0.5)
+        fig.tight_layout()
+        canvas = FigureCanvasTkAgg(fig, master=self._graph_frame)
+        canvas.draw()
+        canvas.get_tk_widget().grid(row=1, column=0, padx=10, pady=10)
 
-if __name__ == "__main__": # execute logic if run directly
-    _root = Tk() # instantiate instance of Tk class
-    _root.title('SPIDAM')
-    _mainframe = ttk.Frame(_root, padding='5 5 5 5 ') # root is parent of frame
-    _mainframe.grid(row=0, column=0, sticky=("E", "W", "N", "S")) # placed on first row,col of parent
-    # frame can extend itself in all cardinal directions
-    _url_frame = ttk.LabelFrame(_mainframe, text='File Path', padding='5 5 5 5') # label frame
-    _url_frame.grid(row=0, column=0, sticky=("E","W")) # only expands E W
-    _url_frame.columnconfigure(0, weight=1)
-    _url_frame.rowconfigure(0, weight=1) # behaves when resizing
+    def funct1(self):
+        print("funct1 test")
 
-    _url = StringVar()
-    _url.set('') # sets initial value of _url
-    _url_entry = ttk.Entry(_url_frame, width=40, textvariable=_url) # text box
-    _url_entry.grid(row=0, column=0, sticky=("E","W", "S", "N"), padx=5)
-    # grid mgr places object at position
+    def funct2(self):
+        mid_freq_data, sr = mm.calculate_mid_freq()
+        for widget in self._graph_frame.winfo_children():
+            widget.destroy()
+        # Create a figure and axis
+        fig, ax = plt.subplots(figsize=(4, 2))
+        # Plot the data
+        librosa.display.specshow(mid_freq_data, sr=sr, x_axis='time', y_axis='log')
+        # Set title and labels
+        ax.set_title("Graph Plot")
+        ax.set_xlabel("Amplitude")
+        ax.set_ylabel("Time(s)")
+        ax.grid(True, linestyle='--', color='gray', linewidth=0.5)
+        # Embed the plot in the Tkinter window
+        canvas = FigureCanvasTkAgg(fig, master=self._graph_frame)  # Create canvas
+        canvas.draw()
+        canvas.get_tk_widget().grid(row=1, column=0, padx=10, pady=10)
 
-    _fetch_btn = ttk.Button(_url_frame, text='Load file', command=funct1) # create button
-    # fetch_url() is callback for button press
-    _fetch_btn.grid(row=0, column=1, sticky=W, padx=5)
+    def funct3(self):
+        print("funct3 test")
 
-    _fetch_btn = ttk.Button(_url_frame, text='Analyze', command=plt_graph)  # create button
-    # fetch_url() is callback for button press
-    _fetch_btn.grid(row=0, column=2, sticky=W, padx=5)
+    def save(self):
+        print("save test")
 
-    _save_method = StringVar()
-    _save_method.set('img')
-    _img_only_radio = ttk.Radiobutton(_url_frame, text='File is .wav', variable=_save_method,
-        value='img')
-    _img_only_radio.grid(row=1, column=0, padx=5, pady=2, sticky="W")
-    _img_only_radio.configure(state='normal')
-    _json_radio = ttk.Radiobutton(_url_frame, text='Convert to .wav', variable=_save_method,
-        value='json')
-    _json_radio.grid(row=2, column=0, padx=5, pady=2, sticky="W")
+    def sb(self,msg):
+        self._status_msg.set(msg)
 
-    _img_frame = ttk.Frame(_mainframe, padding="10")
-    _img_frame.grid(row=1, column=0, padx=10,pady=10)
-    _img_label = ttk.Label(_img_frame, text="Data analysis")
-    _img_label.grid(row=0, column=0)
-    # Create an initial empty plot
-    fig, ax = plt.subplots(figsize=(4, 2))  # Create a blank figure and axis
-    ax.set_title("Graphs")
-    ax.set_xlabel("Time")
-    ax.set_ylabel("Frequency")
-    ax.grid(True)
-    fig.tight_layout()
-
-    # Embed the initial empty plot into Tkinter
-    canvas = FigureCanvasTkAgg(fig, master=_img_frame)
-    canvas.draw()  # Draw the empty plot
-    canvas.get_tk_widget().grid(row=1, column=0,padx=10,pady=10)  # Add the canvas to the grid
-
-    _choice_lbl = ttk.Label(_img_frame, text="Data")
-    _choice_lbl.grid(row=2, column=0, padx=5, pady=5)
-
-    _btn_frame = ttk.Frame(_mainframe, padding=(0, 10, 0, 0))
-    _btn_frame.grid(row=1,column=1)
-
-    _low_btn = ttk.Button(_btn_frame, text='Low', command=funct1)  # create button
-    # fetch_url() is callback for button press
-    _low_btn.grid(row=0, column=0, sticky=W, padx=5)
-    # creates fetch title button
-    _mid_btn = ttk.Button(_btn_frame, text='Medium', command=funct2)  # create button
-    # fetch_url() is callback for button press
-    _mid_btn.grid(row=1, column=0, sticky=W, padx=5)
-
-    # creates fetch link button
-    _high_link_btn = ttk.Button(_btn_frame, text='High', command=funct3)  # create button
-    # fetch_url() is callback for button press
-    _high_link_btn.grid(row=2, column=0, sticky=W, padx=5)
-
-    _status_frame = ttk.Frame(_root, relief='sunken', padding='2 2 2 2')
-    _status_frame.grid(row=1, column=0, sticky=("E", "W", "S",))
-    _status_msg = StringVar()  # need modified when update status text
-    _status_msg.set('Type a file path to select a file...')
-    _status = ttk.Label(_status_frame, textvariable=_status_msg, anchor=W)
-    _status.grid(row=0, column=0, sticky=(E, W))
-
-    _root.mainloop() # listens for events, blocks any code that comes after it
+    def alert(self,msg):
+        messagebox.showinfo(message=msg)
